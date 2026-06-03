a95eebce025306ba4e682811911e0c8703d082e4
julianocremer <j.castorino@escola.pr.gov.br>
2026-06-03 12:20:25 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Fase Lunar carregada 🌙");
+
+// Futuramente você pode integrar uma API lunar aqui.
+// Exemplo:
+// fetch('API_DA_LUA')
+// .then(response => response.json())
+// .then(data => {
+//     document.querySelector('.fase').textContent = data.phase;
+// });
